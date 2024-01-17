d7db8757871634dadfe60dd575edfd1d0b7021c5
Ryan Lane <futile@gmail.com>
2024-01-16 19:22:03 -0800

    stubbing in Inky parts
    just getting this first part working. Will need to make an easier path run on dev machine without inky.

--- utils/eframe_inky.py
+++ utils/eframe_inky.py
@@ -1,2 +1,19 @@
-from PIL import Image, ImageDraw
+from PIL import Image
 from inky.auto import auto
+
+
+inky = auto(ask_user=True, verbose=True)
+
+def get_inky_resolution():
+    return inky.resolution
+
+def show_on_inky(imagepath, saturation = 0.5):
+    # Open the image file and load it into a PIL Image
+    try:
+        image = Image.open(imagepath)
+        inky.set_image(image, saturation=saturation)
+        inky.show()
+    except FileNotFoundError:
+        print(f"Error: Image file not found at {imagepath}")
+    except Exception as e:
+        print(f"Error: Unable to open the image. {e}")
