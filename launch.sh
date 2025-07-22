72bf681a5bf67f1d82fc7b318eeaa1f5a389086f
Ryan Lane <futile@gmail.com>
2025-07-22 10:56:06 -0700

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -7,19 +7,19 @@ fi
 
 # Determine virtualenv path
 if [ "$ENVIRONMENT" == "development" ]; then
-    VENV_PATH="venv"
+    VENV_PATH="./venv"
 else
     VENV_PATH="$HOME/.virtualenvs/movieframe"
 fi
 
-if [ ! -d "$VENV_DIR" ]; then
-  echo "❌ Virtual environment not found at $VENV_DIR"
+if [ ! -d "$VENV_PATH" ]; then
+  echo "❌ Virtual environment not found at $VENV_PATH"
   echo "💡 Run './project-install.sh' to set it up first."
   exit 1
 fi
 
 echo "✅ Activating virtual environment..."
-source "$VENV_DIR/bin/activate"
+source "$VENV_PATH/bin/activate"
 
 echo "🚀 Launching movieplayer..."
 python movieplayer.py
